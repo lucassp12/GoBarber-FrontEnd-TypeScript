b0f0122f8929dfb4ff30edc9a918971af2f79e9d
Lucas dos Santos Pereira <lupereira@fh.com.br>
2020-09-30 22:24:56 -0300

    First Commit

---
+++ src/routes/Route.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+  RouteProps as ReactDOMRouterProps,
+  Route as ReactDOMRoute,
+  Redirect,
+} from 'react-router-dom';
+import { useAuth } from '../hooks/auth';
+
+interface RouterProps extends ReactDOMRouterProps {
+  isPrivate?: boolean;
+  component: React.ComponentType;
+}
+
+const Route: React.FC<RouterProps> = ({
+  isPrivate = false,
+  component: Component,
+  ...rest
+}) => {
+  const { user } = useAuth();
+
+  console.log(user);
+  return (
+    <ReactDOMRoute
+      {...rest}
+      render={() => {
+        return isPrivate === !!user ? (
+          <Component />
+        ) : (
+          <Redirect to={{ pathname: isPrivate ? '/' : '/dashboard' }} />
+        );
+      }}
+    />
+  );
+};
+
+export default Route;
